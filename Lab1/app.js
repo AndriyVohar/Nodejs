eab6758702323ab8a34bcb19bb84296a4a33ca7a
AndriyVohar <vohar.andrii@student.uzhnu.edu.ua>
2024-02-27 16:44:18 +0200

    Update

--- Lab1/app.js
+++ Lab1/app.js
@@ -1,76 +1,106 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const fs = require("fs")
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({  extended: true }));
+const fs = require("fs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//uuid
+const { v4: uuidv4 } = require("uuid");
 
-app.get('/', (req, res) => {
-  fs.readFile(__dirname+'/'+'books.json','utf-8',(err,data)=>{
-    var books = JSON.parse( data );
-    res.end(JSON.stringify(books));
-  })
-})
+app.get("/", (req, res) => {
+  fs.readFile(__dirname + "/" + "books.json", "utf-8", (err, data) => {
+    const books = JSON.parse(data);
+    res.status(200).json(books);
+  });
+});
 
-app.get('/:id', (req, res) => {
-  fs.readFile(__dirname+'/'+'books.json','utf-8',(err,data)=>{
-    var books = JSON.parse( data );
-    var book = books[req.params.id-1]
-    res.end(JSON.stringify(book));
-  })
-})
+app.get("/:id", (req, res) => {
+  fs.readFile(__dirname + "/" + "books.json", "utf-8", (err, data) => {
+    let bookId = req.params.id;
+    const books = JSON.parse(data);
+    const book = books.find((book) => book.id === bookId); // Use strict equality
 
-app.post('/', (req,res) => {
-  fs.readFile( __dirname + "/books.json", 'utf8', (err, data) => {
-    let books = JSON.parse( data );
+    if (book) {
+      res.status(200).json(book);
+    } else {
+      res.status(404).send("Book Not Found");
+    }
+  });
+});
+
+app.post("/", (req, res) => {
+  fs.readFile(__dirname + "/books.json", "utf8", (err, data) => {
+    let books = JSON.parse(data);
     const newBook = {
-      "title": req.body.title,
-      "author": req.body.author,
-      "year": req.body.year,
-      "publisher_address": req.body.publisher_address,
-      "price": req.body.price,
-      "bookstore": req.body.bookstore
+      id: uuidv4(),
+      title: req.body.title,
+      author: req.body.author,
+      year: req.body.year,
+      publisher_address: req.body.publisher_address,
+      price: req.body.price,
+      bookstore: req.body.bookstore,
     };
     books.push(newBook);
-    fs.writeFile(__dirname+'/books.json', JSON.stringify(books,null,2),'utf8',(err)=>{
-      if(err){
-        res.status(500).send('Problem while write in file')
+    fs.writeFile(
+      __dirname + "/books.json",
+      JSON.stringify(books, null, 2),
+      "utf8",
+      (err) => {
+        if (err) {
+          res.status(500).send("Problem while write in file");
+        }
+        res.status(201).send("Created");
       }
-      res.status(201).send('Created')
-    })
- });
-})
+    );
+  });
+});
 
-app.put('/:id', (req,res)=>{
-  fs.readFile( __dirname + "/books.json", 'utf8', (err, data) => {
-    let books = JSON.parse( data );
-    books[req.params.id-1] = req.body;
-    fs.writeFile(__dirname+'/books.json', JSON.stringify(books,null,2),'utf8',(err)=>{
-      if(err){
-        res.status(500).send('Problem while write in file')
+app.put("/:id", (req, res) => {
+  fs.readFile(__dirname + "/books.json", "utf8", (err, data) => {
+    let bookId = req.params.id;
+    let books = JSON.parse(data);
+    const book = books.find((book) => book.id === bookId);
+    const bookIndex = books.findIndex((book) => book.id === bookId);
+    books[bookIndex] = {
+      ...book,
+      ...req.body,
+    };
+    fs.writeFile(
+      __dirname + "/books.json",
+      JSON.stringify(books, null, 2),
+      "utf8",
+      (err) => {
+        if (err) {
+          res.status(500).send("Problem while write in file");
+        }
+        res.status(200).send("Updated");
       }
-      res.status(200).send('Updated')
-    })
- });
-})
+    );
+  });
+});
 
-app.delete('/:id', (req,res)=>{
-  fs.readFile( __dirname + "/books.json", 'utf8', (err, data) => {
-    let books = JSON.parse( data )
-    delete books[req.params.id-1]
-    books = books.filter(Boolean)
-    fs.writeFile(__dirname+'/books.json', JSON.stringify(books,null,2),'utf8',(err)=>{
-      if(err){
-        res.status(500).send('Problem while write in file')
+app.delete("/:id", (req, res) => {
+  fs.readFile(__dirname + "/books.json", "utf8", (err, data) => {
+    let bookId = req.params.id;
+    let books = JSON.parse(data);
+    books = books.filter((book) => book.id !== req.params.id);
+    fs.writeFile(
+      __dirname + "/books.json",
+      JSON.stringify(books, null, 2),
+      "utf8",
+      (err) => {
+        if (err) {
+          res.status(500).send("Problem while write in file");
+        }
+        res.status(200).send("Deleted");
       }
-      res.status(200).send('Deleted')
-    })
- });
-})
+    );
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
